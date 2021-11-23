e5da2bf09b3634e8f9d1eee2512a63b7db97a5c4
Stephen <steve.wiggins7@gmail.com>
2021-11-22 23:35:02 -0500

    users controller with routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+
+        render json: @users, status: :ok
+    end
+
+
+    def create
+        @user = User.new(user_params)
+
+        render json: @user, status: :created
+    end
+
+    def destroy
+        @user = User.where(id: params[:id]).first
+        if @contact.destroy
+            head(:ok)
+        else
+            head(:unproccessable_entity)
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
