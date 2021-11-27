65f8b3d0af81e76d721b0d3556464c5156207627
Stephen <steve.wiggins7@gmail.com>
2021-11-27 00:18:17 -0500

    day controller code

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,6 +1,25 @@
 class DaysController < ApplicationController
 
-    def create
-        day = User.days.create()
+    def index
+        days = Day.all
+
+        render json: days, status: :ok
     end
+
+
+    def create
+        day= Day.create(params.require(:user).permit(:user_id, :date, :content))
+
+        render json: day, status: :created
+    end
+
+    def destroy
+        day= Day.where(id: params[:id]).first
+        if day.destroy
+            head(:ok)
+        else
+            head(:unproccessable_entity)
+        end
+    end
+
 end
